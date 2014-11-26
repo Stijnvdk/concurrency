6315c6a8a07f6ad027fb19c3a61eb8165f98baed
Stijnvdk <stijn.van.der.kroon@protopics.nl>
2014-11-26 13:52:21 +0100

    Dingen

--- Concurrency1/src/Main.java
+++ Concurrency1/src/Main.java
@@ -2,4 +2,24 @@
  * Created by Stijn on 26-11-2014.
  */
 public class Main {
+
+    public static void main (String args[]){
+        createAndLogForAantal(25000);
+        createAndLogForAantal(50000);
+        createAndLogForAantal(100000);
+        createAndLogForAantal(200000);
+        createAndLogForAantal();
+    }
+
+    public static void createAndLogForAantal(int aantal){
+
+        long start = System.currentTimeMillis();
+
+        int[] arrayToSort = ArrayGenerator.createArray(aantal);
+        Sorter.insertionSort(arrayToSort);
+
+        long end = System.currentTimeMillis();
+        System.out.println("Done sorting, took " + (end - start) + " milisecs");
+    }
+
 }
--- Concurrency1/src/Sorter.java
+++ Concurrency1/src/Sorter.java
@@ -2,4 +2,23 @@
  * Created by Stijn on 26-11-2014.
  */
 public class Sorter {
+
+
+    /**
+     * Bron: http://www.algolist.net/Algorithms/Sorting/Insertion_sort
+     * @param arrayToSort
+     * @return
+     */
+   public static void insertionSort(int[] arrayToSort) {
+        int i, j, newValue;
+        for (i = 1; i < arrayToSort.length; i++) {
+            newValue = arrayToSort[i];
+            j = i;
+            while (j > 0 && arrayToSort[j - 1] > newValue) {
+                arrayToSort[j] = arrayToSort[j - 1];
+                j--;
+            }
+            arrayToSort[j] = newValue;
+        }
+    }
 }
