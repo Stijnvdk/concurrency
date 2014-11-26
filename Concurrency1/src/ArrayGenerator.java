4d399f39b019d34984650d74aee821794da0f807
Stijnvdk <stijn.van.der.kroon@protopics.nl>
2014-11-26 13:42:32 +0100

    Werkende array generator

--- Concurrency1/src/ArrayGenerator.java
+++ Concurrency1/src/ArrayGenerator.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 /**
  * Created by Stijn on 26-11-2014.
  */
 public class ArrayGenerator {
+
+    private static Random random = new Random();
+
+
+    public static int[] createArray(Integer aantal){
+        int[] result = new int[aantal];
+        for (int i = 0; i < aantal; i++)
+        {
+            result[i] = random.nextInt(100);
+        }
+        return result;
+    }
+
 }
